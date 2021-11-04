cd5b55aee0660cb60991d2bba4d0095622cf6e6f
Edixon van Vliet <40107868+Edixon-vanVliet@users.noreply.github.com>
2021-11-04 15:08:15 -0400

    Feature/bottom navigation (#1)
    * Install paper
    
    * Add vector icons
    
    * Configure paper
    
    * Create empty screens
    
    * Create bottom navigation
    
    * Modify styles and removed status bar
    
    * Add paper provider
    
    * Add bottom navigation
    
    * Remove unused declaration

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import React from "react";
+import { StatusBar, StyleSheet, View } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
+import { BottomNavigation } from "./components/BottomNavigation";
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: StatusBar.currentHeight,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <View style={styles.container}>
+        <BottomNavigation />
+      </View>
+    </PaperProvider>
+  );
+}
