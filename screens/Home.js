08214fb80b161fbeb7fb7feeaf6165b4c72434f5
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2021-11-03 22:45:26 -0400

    Create empty screens

---
+++ screens/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export const Home = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Inicio</Text>
+    </View>
+  );
+};
---
+++ screens/Search.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export const Search = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Buscar</Text>
+    </View>
+  );
+};
