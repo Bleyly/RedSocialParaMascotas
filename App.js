6a7e9b04ab98ff1cd68cd4e18acfec8c8cd7fe78
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2021-11-06 02:36:45 -0400

    Implement drawer and tab navigation

--- App.js
+++ App.js
@@ -1,7 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import { StatusBar, StyleSheet, View } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
-import { Appbar, BottomNavigation } from "./components";
+import {
+  Provider as PaperProvider,
+  DefaultTheme as PaperDefaultTheme,
+} from "react-native-paper";
+import { DefaultTheme as NavigationDefaultTheme } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { TabNavigation } from "./src/components/navigation/TabNavigation";
+import { DrawerNavigation } from "./src/components/navigation/DrawerNavigation";
+
+const Drawer = createDrawerNavigator();
+
+const App = () => {
+  return (
+    <PaperProvider>
+      <View style={styles.container}>
+        <NavigationContainer theme={theme}>
+          <Drawer.Navigator
+            initialRouteName="TabNavigation"
+            screenOptions={{ headerShown: false }}
+            drawerContent={DrawerNavigation}
+          >
+            <Drawer.Screen name="TabNavigation" component={TabNavigation} />
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </View>
+    </PaperProvider>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -10,15 +37,15 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function App() {
-  const [title, setTitle] = useState("Inicio");
+const theme = {
+  ...NavigationDefaultTheme,
+  colors: {
+    ...NavigationDefaultTheme.colors,
+    background: PaperDefaultTheme.colors.background,
+    notification: PaperDefaultTheme.colors.notification,
+    primary: PaperDefaultTheme.colors.primary,
+    text: PaperDefaultTheme.colors.text,
+  },
+};
 
-  return (
-    <PaperProvider>
-      <View style={styles.container}>
-        <Appbar title={title} />
-        <BottomNavigation setTitle={setTitle} />
-      </View>
-    </PaperProvider>
-  );
-}
+export default App;
