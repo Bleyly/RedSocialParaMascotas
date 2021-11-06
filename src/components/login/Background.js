2d412fdab1cfe843d71f6c86c321ef6e2ff1ec5a
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2021-11-06 16:17:45 -0400

    Create background for login

---
+++ src/components/login/Background.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  ImageBackground,
+  KeyboardAvoidingView,
+  StyleSheet,
+} from "react-native";
+
+export const Background = ({ children }) => {
+  return (
+    <ImageBackground
+      source={require("../../../assets/login/background_dot.png")}
+      resizeMode="repeat"
+      style={styles.background}
+    >
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        {children}
+      </KeyboardAvoidingView>
+    </ImageBackground>
+  );
+};
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: "100%",
+  },
+  container: {
+    flex: 1,
+    padding: 20,
+    width: "100%",
+    maxWidth: 340,
+    alignSelf: "center",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
