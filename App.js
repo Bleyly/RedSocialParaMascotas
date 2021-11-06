3edb33fdb656d45d7a4c9f7c2476b511a4e58e1d
Edixon van Vliet <40107868+Edixon-vanVliet@users.noreply.github.com>
2021-11-06 15:00:48 -0400

    Fix/navigation (#3)
    * Move components to src
    
    * Move screens to library
    
    * Move screens to src
    
    * Update route
    
    * Remove files to refactor
    
    * Install react-navigations
    
    * Create tab navigation
    
    * Create drawer content
    
    * Implement drawer and tab navigation
    
    * Fix typo
    
    * Replaced magic strings for object properties
    
    * Enable navigation to home on drawer
    
    * Create helper to get header title
    
    * Move app bar to drawer

--- App.js
+++ App.js
@@ -1,7 +1,50 @@
-import React, { useState } from "react";
+import React from "react";
 import { StatusBar, StyleSheet, View } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
-import { Appbar, BottomNavigation } from "./components";
+import {
+  Provider as PaperProvider,
+  DefaultTheme as PaperDefaultTheme,
+  Appbar,
+} from "react-native-paper";
+import { DefaultTheme as NavigationDefaultTheme } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { TabNavigation } from "./src/components/navigation/TabNavigation";
+import { DrawerNavigation } from "./src/components/navigation/DrawerNavigation";
+import { getHeaderTitle } from "./src/helpers/getHeaderTitle";
+
+const Drawer = createDrawerNavigator();
+
+const App = () => {
+  return (
+    <PaperProvider>
+      <View style={styles.container}>
+        <NavigationContainer theme={theme}>
+          <Drawer.Navigator
+            initialRouteName="TabNavigation"
+            drawerContent={DrawerNavigation}
+            screenOptions={{
+              header: ({ options: { title }, navigation: { openDrawer } }) => {
+                return (
+                  <Appbar>
+                    <Appbar.Action icon="menu" onPress={openDrawer} />
+                    <Appbar.Content title={title} />
+                    <Appbar.Action icon="chat" />
+                  </Appbar>
+                );
+              },
+            }}
+          >
+            <Drawer.Screen
+              name="TabNavigation"
+              component={TabNavigation}
+              options={({ route }) => ({ title: getHeaderTitle(route) })}
+            />
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </View>
+    </PaperProvider>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -10,15 +53,15 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function App() {
-  const [title, setTitle] = useState("Inicio");
+const theme = {
+  ...NavigationDefaultTheme,
+  colors: {
+    ...NavigationDefaultTheme.colors,
+    background: PaperDefaultTheme.colors.background,
+    notification: PaperDefaultTheme.colors.notification,
+    primary: PaperDefaultTheme.colors.primary,
+    text: PaperDefaultTheme.colors.text,
+  },
+};
 
-  return (
-    <PaperProvider>
-      <View style={styles.container}>
-        <Appbar title={title} />
-        <BottomNavigation setTitle={setTitle} />
-      </View>
-    </PaperProvider>
-  );
-}
+export default App;
