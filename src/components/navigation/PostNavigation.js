05039223a2a6f0a08293cc2458c3f3f89a37dd87
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2021-11-10 00:03:05 -0400

    Add standard post to navigation

--- src/components/navigation/PostNavigation.js
+++ src/components/navigation/PostNavigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { names, Post } from "../../screens";
+import { StandarPost } from "../../screens/StandarPost";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export const PostNavigation = () => {
       screenOptions={{ headerShown: false }}
     >
       <Stack.Screen name={names.post} component={Post} />
+      <Stack.Screen name={names.standardPost} component={StandarPost} />
     </Stack.Navigator>
   );
 };
