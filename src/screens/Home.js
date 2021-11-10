31995eeedccba4942f785015ae3f5281d11c8613
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2021-11-10 19:00:30 -0400

    Move home components to its own folder

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,21 +1,18 @@
 import React from "react";
 import { ScrollView, View } from "react-native";
-import { Cards } from "../components/Card";
+import { Cards } from "../components/home";
 
 export const Home = () => {
   return (
     <ScrollView>
       <View
-        style={
-          {
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-          }
-        }  
-      >
-      </View>
-      <Cards/>
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      ></View>
+      <Cards />
     </ScrollView>
   );
 };
