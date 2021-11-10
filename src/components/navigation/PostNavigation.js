46c42d1a37bd485c7e7c5ff31e3c24e1468a3f62
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2021-11-10 01:11:21 -0400

    Add selling post to navigation

--- src/components/navigation/PostNavigation.js
+++ src/components/navigation/PostNavigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { names, Post } from "../../screens";
 import { StandarPost } from "../../screens/StandarPost";
+import { VenderPost } from "../../screens/VenderPost";
 
 const Stack = createNativeStackNavigator();
 
@@ -13,6 +14,7 @@ export const PostNavigation = () => {
     >
       <Stack.Screen name={names.post} component={Post} />
       <Stack.Screen name={names.standardPost} component={StandarPost} />
+      <Stack.Screen name={names.sellPost} component={VenderPost} />
     </Stack.Navigator>
   );
 };
