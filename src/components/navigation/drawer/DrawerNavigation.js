fa2311ae4c050a30cf6c39c12fdeae2781968791
Edixon van Vliet <40107868+Edixon-vanVliet@users.noreply.github.com>
2021-12-05 14:35:39 -0400

    Agregar pantalla de chat

--- src/components/navigation/drawer/DrawerNavigation.js
+++ src/components/navigation/drawer/DrawerNavigation.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Appbar } from "react-native-paper";
 import { getHeaderTitle } from "../../../helpers/getHeaderTitle";
 import { names, titles } from "../../../screens";
+import { Chats } from "../../../screens/chat/Chats";
 import { Profile } from "../../../screens/Profile";
 import { TabNavigation } from "../TabNavigation";
 import { DrawerContent } from "./DrawerContent";
@@ -10,35 +11,42 @@ import { DrawerContent } from "./DrawerContent";
 const Drawer = createDrawerNavigator();
 
 export const DrawerNavigation = () => {
-  return (
-    <Drawer.Navigator
-      initialRouteName="TabNavigation"
-      drawerContent={(props) => <DrawerContent {...props} />}
-      screenOptions={{
-        header: ({ options: { title }, navigation: { openDrawer } }) => {
-          return (
-            <Appbar>
-              <Appbar.Action icon="menu" onPress={openDrawer} />
-              <Appbar.Content title={title} />
-              <Appbar.Action icon="chat" />
-            </Appbar>
-          );
-        },
-      }}
-    >
-      <Drawer.Screen
-        name="TabNavigation"
-        component={TabNavigation}
-        options={({ route }) => ({ title: getHeaderTitle(route) })}
-      />
-      <Drawer.Screen
-        name={names.profile}
-        component={Profile}
-        options={{
-          title: titles[names.profile],
-          unmountOnBlur: true,
-        }}
-      />
-    </Drawer.Navigator>
-  );
+	return (
+		<Drawer.Navigator
+			initialRouteName="TabNavigation"
+			drawerContent={(props) => <DrawerContent {...props} />}
+			screenOptions={{
+				header: ({
+					options: { title },
+					navigation: { openDrawer, navigate },
+				}) => {
+					return (
+						<Appbar>
+							<Appbar.Action icon="menu" onPress={openDrawer} />
+							<Appbar.Content title={title} />
+							<Appbar.Action
+								icon="chat"
+								onPress={() => navigate("chat")}
+							/>
+						</Appbar>
+					);
+				},
+			}}
+		>
+			<Drawer.Screen
+				name="TabNavigation"
+				component={TabNavigation}
+				options={({ route }) => ({ title: getHeaderTitle(route) })}
+			/>
+			<Drawer.Screen
+				name={names.profile}
+				component={Profile}
+				options={{
+					title: titles[names.profile],
+					unmountOnBlur: true,
+				}}
+			/>
+			<Drawer.Screen name="chat" component={Chats} />
+		</Drawer.Navigator>
+	);
 };
---
+++ src/screens/chat/Chats.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export const Chats = () => {
+	return (
+		<View>
+			<Text> La creta </Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		marginTop: 8,
+		backgroundColor: "white",
+	},
+});
