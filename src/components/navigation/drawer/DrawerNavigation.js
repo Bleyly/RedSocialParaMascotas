edbfa6a36663b9f2ef1ae31ca1bbf1d96e959422
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2021-12-07 19:27:45 -0400

    Merge branch 'dev' of https://github.com/Bleyly/RedSocialParaMascotas into feature/chat

--- src/components/navigation/drawer/DrawerNavigation.js
+++ src/components/navigation/drawer/DrawerNavigation.js
@@ -1,11 +1,12 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import React from "react";
 import { Appbar } from "react-native-paper";
+import { PicturesProvider } from "../../../helpers/picturesContext";
 import { getHeaderTitle } from "../../../helpers/getHeaderTitle";
 import { names, titles } from "../../../screens";
-import { Chats } from "../../../screens/chat/Chats";
 import { Profile } from "../../../screens/Profile";
 import { ChatNavigation } from "../ChatNavigation";
+import { Camera } from "../../../screens/Camera";
 import { TabNavigation } from "../TabNavigation";
 import { DrawerContent } from "./DrawerContent";
 
@@ -13,41 +14,51 @@ const Drawer = createDrawerNavigator();
 
 export const DrawerNavigation = () => {
 	return (
-		<Drawer.Navigator
-			initialRouteName="TabNavigation"
-			drawerContent={(props) => <DrawerContent {...props} />}
-			screenOptions={{
-				header: ({
-					options: { title },
-					navigation: { openDrawer, navigate },
-				}) => {
-					return (
-						<Appbar>
-							<Appbar.Action icon="menu" onPress={openDrawer} />
-							<Appbar.Content title={title} />
-							<Appbar.Action
-								icon="chat"
-								onPress={() => navigate("ChatNavigation")}
-							/>
-						</Appbar>
-					);
-				},
-			}}
-		>
-			<Drawer.Screen
-				name="TabNavigation"
-				component={TabNavigation}
-				options={({ route }) => ({ title: getHeaderTitle(route) })}
-			/>
-			<Drawer.Screen
-				name={names.profile}
-				component={Profile}
-				options={{
-					title: titles[names.profile],
-					unmountOnBlur: true,
-				}}
-			/>
-			<Drawer.Screen name="ChatNavigation" component={ChatNavigation} />
-		</Drawer.Navigator>
+    <PicturesProvider>
+      <Drawer.Navigator
+        initialRouteName="TabNavigation"
+        drawerContent={(props) => <DrawerContent {...props} />}
+        screenOptions={{
+          header: ({
+            options: { title },
+            navigation: { openDrawer, navigate },
+          }) => {
+            return (
+              <Appbar>
+                <Appbar.Action icon="menu" onPress={openDrawer} />
+                <Appbar.Content title={title} />
+                <Appbar.Action
+                  icon="chat"
+                  onPress={() => navigate("ChatNavigation")}
+                />
+              </Appbar>
+            );
+          },
+        }}
+      >
+        <Drawer.Screen
+          name="TabNavigation"
+          component={TabNavigation}
+          options={({ route }) => ({ title: getHeaderTitle(route) })}
+        />
+        <Drawer.Screen
+          name={names.profile}
+          component={Profile}
+          options={{
+            title: titles[names.profile],
+            unmountOnBlur: true,
+          }}
+        />
+        <Drawer.Screen
+            name={names.camera}
+            component={Camera}
+            options={{
+              headerShown: false,
+              unmountOnBlur: true,
+            }}
+          />
+        <Drawer.Screen name="ChatNavigation" component={ChatNavigation} />
+      </Drawer.Navigator>
+    </PicturesProvider>
 	);
 };
