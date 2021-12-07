11df0fd861c0deab439416fdaf080d8f2d8e811f
Edixon van Vliet <40107868+Edixon-vanVliet@users.noreply.github.com>
2021-12-07 19:13:31 -0400

    Feature/picture picker (#17)
    * Add package expo camera
    
    * Create a context for pictures
    
    * Create camera screen
    
    * Implement camera
    
    * Add package expo image picker
    
    * Manage picker

---
+++ src/hooks/usePicker.js
@@ -0,0 +1,32 @@
+import React from "react";
+import * as ImagePicker from "expo-image-picker";
+import { Platform } from "react-native";
+import { usePicturesContext } from "../helpers/picturesContext";
+
+export const usePicker = () => {
+  const {
+    picturesState: [, savePicture],
+  } = usePicturesContext();
+
+  const pickPicture = async () => {
+    if (Platform.OS !== "web") {
+      const { status } =
+        await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+      if (status === "granted") {
+        const { uri } = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [1, 1],
+          quality: 1,
+        });
+
+        savePicture([{ uri }]);
+      } else {
+        alert("Lo sentimos, necesitamos permiso a la galleria!");
+      }
+    }
+  };
+
+  return pickPicture;
+};
