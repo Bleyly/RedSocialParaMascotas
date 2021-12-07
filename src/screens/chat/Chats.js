52f79c74e6d4d4bf5bf182e17e2c1d4b1d601179
Edixon van Vliet <40107868+Edixon-vanVliet@users.noreply.github.com>
2021-12-07 18:11:55 -0400

    Crear pantalla de chats

--- src/screens/chat/Chats.js
+++ src/screens/chat/Chats.js
@@ -1,11 +1,34 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { Fragment } from "react";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { Avatar, Caption, Title, TouchableRipple } from "react-native-paper";
+import { chats } from "../../../data/chats";
 
-export const Chats = () => {
+export const Chats = ({ navigation: { navigate } }) => {
 	return (
-		<View>
-			<Text> La creta </Text>
-		</View>
+		<ScrollView style={styles.container}>
+			<TouchableRipple onPress={() => navigate("chat")}>
+				<View style={styles.chat}>
+					<Avatar.Image source={chats[0].userImg} />
+					<View style={styles.information}>
+						<Title>{chats[0].userName}</Title>
+						<View style={{ flexDirection: "row" }}>
+							<Caption
+								ellipsizeMode="tail"
+								numberOfLines={1}
+								style={{ flexBasis: "60%" }}
+							>
+								{chats[0].lastMessage}
+							</Caption>
+							<Caption
+								style={{ marginLeft: 4, flexBasis: "40%" }}
+							>
+								{chats[0].messageTime}
+							</Caption>
+						</View>
+					</View>
+				</View>
+			</TouchableRipple>
+		</ScrollView>
 	);
 };
 
@@ -14,5 +37,15 @@ const styles = StyleSheet.create({
 		flex: 1,
 		marginTop: 8,
 		backgroundColor: "white",
+	},
+	chat: {
+		margin: 16,
+		flexDirection: "row",
+		borderBottomWidth: 1.5,
+		borderBottomColor: "#e0e0e0",
+		paddingBottom: 8,
+	},
+	information: {
+		marginLeft: 8,
 	},
 });
