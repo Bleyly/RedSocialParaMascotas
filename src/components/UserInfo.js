80364faac0489938e75d49a523b9c3902856eb5f
Edixon van Vliet <Edixon.vanVliet@hotmail.com>
2021-12-09 02:17:40 -0400

    Add on press event

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { View } from "react-native";
 import { StyleSheet } from "react-native";
-import { Avatar, Title } from "react-native-paper";
+import { Avatar, Title, TouchableRipple } from "react-native-paper";
 
-export const UserInfo = ({ image, name, style }) => {
+export const UserInfo = ({ image, name, style, onPress = () => {} }) => {
   return (
-    <View style={[styles.userInfo, style]}>
-      <Avatar.Image source={image} style={styles.image} />
-      <Title style={styles.name}>{name}</Title>
-    </View>
+    <TouchableRipple
+      onPress={onPress}
+      style={{ paddingTop: 8, paddingBottom: 8 }}
+    >
+      <View style={[styles.userInfo, style]}>
+        <Avatar.Image source={image} style={styles.image} />
+        <Title style={styles.name}>{name}</Title>
+      </View>
+    </TouchableRipple>
   );
 };
 
 const styles = StyleSheet.create({
   userInfo: { flexDirection: "row", alignItems: "center" },
-  image: { marginTop: 16 },
   name: { marginLeft: 16 },
 });
