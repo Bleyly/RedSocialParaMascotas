c25f71f8697b375c74912e366f3286e8f5d631bd
Edixon van Vliet <40107868+Edixon-vanVliet@users.noreply.github.com>
2021-12-09 02:15:30 -0400

    Cambiar opcion

--- src/screens/setting/Settings.js
+++ src/screens/setting/Settings.js
@@ -6,85 +6,79 @@ import { ProgressBar } from "../../components/Post";
 import { names } from "../names";
 
 export const Settings = ({ navigation: { navigate } }) => {
-	return (
-		<View style={styles.container}>
-			<Title style={styles.title}>Configuración</Title>
-			<ProgressBar />
+  return (
+    <View style={styles.container}>
+      <Title style={styles.title}>Configuración</Title>
+      <ProgressBar />
 
-			<View style={styles.options}>
-				<List.Section>
-					<TouchableRipple
-						onPress={() => navigate(names.edit_profile)}
-					>
-						<List.Item
-							title="Editar Perfil"
-							left={() => (
-								<Icon
-									size={32}
-									source={require("../../../assets/iconsSetting/icons_EditarPerfil.png")}
-								/>
-							)}
-						/>
-					</TouchableRipple>
-					<TouchableRipple
-						onPress={() => navigate(names.personal_information)}
-					>
-						<List.Item
-							title="Información Personal"
-							left={() => (
-								<Icon
-									size={32}
-									source={require("../../../assets/iconsSetting/icon_InformacionPersonal.png")}
-								/>
-							)}
-						/>
-					</TouchableRipple>
-					<TouchableRipple onPress={() => navigate(names.post_liked)}>
-						<List.Item
-							title="Publicación que te gustó"
-							left={() => (
-								<Icon
-									size={32}
-									source={require("../../../assets/iconsSetting/icon_meGusta.png")}
-								/>
-							)}
-						/>
-					</TouchableRipple>
-					<TouchableRipple
-						onPress={() => navigate(names.account_status)}
-					>
-						<List.Item
-							title="Estado de la cuenta"
-							left={() => (
-								<Icon
-									size={32}
-									source={require("../../../assets/iconsSetting/icon._estadoDeCuenta.png")}
-								/>
-							)}
-						/>
-					</TouchableRipple>
-				</List.Section>
-			</View>
-		</View>
-	);
+      <View style={styles.options}>
+        <List.Section>
+          <TouchableRipple onPress={() => navigate(names.edit_profile)}>
+            <List.Item
+              title="Editar Perfil"
+              left={() => (
+                <Icon
+                  size={32}
+                  source={require("../../../assets/iconsSetting/icons_EditarPerfil.png")}
+                />
+              )}
+            />
+          </TouchableRipple>
+          <TouchableRipple onPress={() => navigate(names.personal_information)}>
+            <List.Item
+              title="Información Personal"
+              left={() => (
+                <Icon
+                  size={32}
+                  source={require("../../../assets/iconsSetting/icon_InformacionPersonal.png")}
+                />
+              )}
+            />
+          </TouchableRipple>
+          <TouchableRipple onPress={() => navigate(names.post_liked)}>
+            <List.Item
+              title="Publicación que te gustó"
+              left={() => (
+                <Icon
+                  size={32}
+                  source={require("../../../assets/iconsSetting/icon_meGusta.png")}
+                />
+              )}
+            />
+          </TouchableRipple>
+          <TouchableRipple onPress={() => navigate(names.account_status)}>
+            <List.Item
+              title="Cambiar Contraseña"
+              left={() => (
+                <Icon
+                  size={32}
+                  source={require("../../../assets/iconsSetting/icon._estadoDeCuenta.png")}
+                />
+              )}
+            />
+          </TouchableRipple>
+        </List.Section>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		marginTop: 8,
-		backgroundColor: "white",
-	},
-	title: {
-		fontSize: 24,
-		marginLeft: 16,
-		marginBottom: 12,
-		marginTop: 12,
-	},
-	options: {
-		flex: 1,
-		marginTop: 24,
-		paddingLeft: 16,
-		paddingRight: 16,
-	},
+  container: {
+    flex: 1,
+    marginTop: 8,
+    backgroundColor: "white",
+  },
+  title: {
+    fontSize: 24,
+    marginLeft: 16,
+    marginBottom: 12,
+    marginTop: 12,
+  },
+  options: {
+    flex: 1,
+    marginTop: 24,
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
 });
